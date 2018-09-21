7a533bdd61c9212d47072175698d585ebc08479b
Алиев Санат Кайратович <Sanat.Aliyev@kaspi.kz>
2018-09-21 19:17:38 +0600

    more

--- 1.JavaSyntax/src/com/javarush/task/task04/task0415/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0415/Solution.java
@@ -5,10 +5,22 @@ package com.javarush.task.task04.task0415;
 */
 
 import java.io.*;
+import java.nio.Buffer;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
         //TODO todo
         //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+
+        if ((a + b) <= c || (a + c) <= b || (b + c) <= a) {
+            System.out.println("Треугольник не существует.");
+        } else {
+            System.out.println("Треугольник существует.");
+        }
     }
 }
